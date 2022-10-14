4e85b1a30cf12074e66106d901f833eb0ba08bbb
Oliveiralucass <gf.oliveiralucas@gmail.com>
2022-10-14 10:50:11 -0300

    Adicionado homework 8

---
+++ aula7/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require ("copy-webpack-plugin")
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      
+    ]
+  },
+  plugins: [new MiniCssExtractPlugin({
+      filename: 'style.css'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './index.html',
+      hash: true
+    }),
+    new CopyPlugin({
+      patterns: [
+        {from: "./img", to: "./img"}
+      ]
+    }),
+  ],
+    devServer: {
+      static: path.resolve(__dirname, 'dist'),
+      port: 3000
+    }
+}
